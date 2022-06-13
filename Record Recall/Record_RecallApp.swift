775528fc1110d1cb748090b4ba58e36d0234d774
Justin Nipper <justin.nipper@me.com>
2022-06-13 14:23:57 -0500

    Initial Commit

---
+++ Record Recall/Record_RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_RecallApp.swift
+//  Record Recall
+//
+//  Created by Justin Nipper on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Record_RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
