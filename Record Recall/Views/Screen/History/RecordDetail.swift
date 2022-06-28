64717b17b45cddc45f6724aaed0ec102e0f25dd6
Justin Nipper <justin.nipper@me.com>
2022-06-28 13:13:54 -0500

    Reorganizes files and creates detail view for records

--- Record Recall/Views/Screen/History/RecordDetail.swift
+++ Record Recall/Views/Screen/History/RecordDetail.swift
@@ -6,20 +6,72 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct RecordDetail: View {
     var record: Record
+    @FetchRequest var fetchRequest: FetchedResults<Record>
+    
     var body: some View {
-        VStack {
-            Text((record.exercise?.name)!)
-            if record.date != nil { Text(Helper.getFriendlyDateString(from: record.date!))
-                    .font(.footnote)
-                    .fontWeight(.medium)
-                    .foregroundColor(.secondaryBlue)
+        List(fetchRequest, id:\.self) { record in
+            VStack(alignment: .leading) {
+                if record.date != nil {
+                    HStack {
+                        HStack {
+                            if (!(Metric(value: record.weight).formattedValue == "0") || record.weight > 0) {
+                                HStack(spacing: 2) {
+                                    Text(Metric(value: record.weight).formattedValue)
+                                        .font(.system(size: 24, weight: .semibold))
+                                        .foregroundColor(.primaryBlue)
+                                    Text(record.unit == Unit.metric.rawValue ? "kg" : "lb")
+                                        .font(.system(size: 12, weight: .medium))
+                                        .foregroundColor(.secondaryBlue)
+                                }
+                                
+                            }
+                            if (!(Metric(value: record.weight).formattedValue == "0") || record.weight > 0) {
+                                Text("x")
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .foregroundColor(.primaryBlue)
+                            }
+                            HStack(spacing: 2) {
+                                Text("\(record.reps, specifier: "%.0f")")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.primaryBlue)
+                                
+                                Text("reps")
+                                    .font(.system(size: 12, weight: .medium))
+                                    .foregroundColor(.secondaryBlue)
+                            }
+                        }
+                        Spacer()
+                        Text(Helper.getFriendlyDateString(from: record.date!))
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(.secondaryBlue)
+                    }
+                    .padding(.top, 8)
+                }
+                if !record.notes!.isEmpty {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Notes")
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .italic()
+                            .foregroundColor(.primaryBlue)
+                        Text(record.notes ?? "")
+                            .font(.system(size: 14, weight: .regular))
+                            .foregroundColor(.primaryBlue)
+                    }
+                    .padding(.vertical, 8)
+                }
             }
-            WeightRepsView(record: record)
-            NotesView(record: record)
-            Spacer()
         }
+        .navigationTitle((record.exercise?.name)!)
+    }
+    
+    init(record: Record) {
+        self.record = record
+        _fetchRequest = FetchRequest<Record>(sortDescriptors: [], predicate: NSPredicate(format: "%K CONTAINS[cd] %@", #keyPath(Record.exercise.name), (self.record.exercise?.name!)!))
     }
 }
