63148c2a88d9f778a5b4554ec59784477714b571
Justin Nipper <justin.nipper@me.com>
2022-07-10 22:03:09 -0500

    Adds AdMob to app home screen

--- Record Recall/ContentView.swift
+++ Record Recall/ContentView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import AppTrackingTransparency
+import GoogleMobileAds
 
 struct ContentView: View {
     var body: some View {
         AppTabView()
             .preferredColorScheme(.light)
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                setupAdsTracking()
+            }
+        
+    }
+    
+    func setupAdsTracking() {
+        ATTrackingManager.requestTrackingAuthorization { status in
+            switch status {
+            case .notDetermined:
+                break
+            case .restricted:
+                break
+            case .denied:
+                break
+            case .authorized:
+                GADMobileAds.sharedInstance().start(completionHandler: nil)
+                break
+            @unknown default:
+                break
+            }
+        }
     }
 }
 
