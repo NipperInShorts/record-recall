521a1108f1085082db627321efc6f24324ddd16a
Justin Nipper <justin.nipper@me.com>
2022-07-11 11:57:59 -0500

    Adds header warning about deleting records

--- Record Recall/Views/Screen/Settings/ExerciseManagementView.swift
+++ Record Recall/Views/Screen/Settings/ExerciseManagementView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ExerciseManagementView: View {
     @StateObject var viewModel = AddExerciseViewModel()
     @State private var showPopover = false
-    @State private var isShowingDeleteAction = false
     @Environment(\.dismiss) var dismiss
     @FetchRequest(fetchRequest: Exercise.allExercises)
     var exercises: FetchedResults<Exercise>
@@ -20,38 +19,26 @@ struct ExerciseManagementView: View {
                 EmptyScreenMessage(title: "Glad you're here.", message: "The exercise list is empty.\nYou can add exercises by using the plus button at the top.")
             } else {
                 List {
-                    ForEach(exercises) { exercise in
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text(exercise.name!)
-                                    .foregroundColor(.primaryBlue)
+                    Section {
+                        ForEach(exercises) { exercise in
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text(exercise.name!)
+                                        .foregroundColor(.primaryBlue)
+                                }
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button(role: .destructive) {
+                                    delete(exercise: exercise)
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
                             }
                         }
-                        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
-                            Button(role: .destructive) {
-                                isShowingDeleteAction = true
-                            } label: {
-                                Label("Delete", systemImage: "trash")
-                            }
-                        }
-                        .confirmationDialog("Deleting Exercise Removes Records", isPresented: $isShowingDeleteAction) {
-                            Button(role: .destructive) {
-                                delete(exercise: exercise)
-                                isShowingDeleteAction = false
-                            } label: {
-                                Text("Delete")
-                            }
-                            Button(role: .cancel) {
-                                //handle delete
-                                isShowingDeleteAction = false
-                            } label: {
-                                Text("Cancel")
-                            }
-                        } message: {
-                            Text("Deleting the exercises removes associated records")
-                        }
+                    } header: {
+                        Text("Deleting exercises will remove their associated records")
+                            .foregroundColor(.red)
                     }
-                    
                 }
             }
         }
@@ -61,18 +48,7 @@ struct ExerciseManagementView: View {
         }
         .background(Color.backgroundBlue)
         .navigationTitle("Exercises")
-        .navigationBarBackButtonHidden(true)
         .navigationBarItems(
-            leading:
-                Button {
-                    dismiss()
-                } label: {
-                    HStack {
-                        Image(systemName: "chevron.left")
-                            .font(.system(size: 14, weight: .semibold))
-                        Text("Back")
-                    }
-                },
             trailing:
                 Button  {
                     showPopover.toggle()
